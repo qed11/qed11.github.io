40145ed01ddfce2300b46a6e38576d83f84c7a13
qed11 <ericdai1130@gmail.com>
2022-06-24 16:19:25 -0400

    added functions to compute global and local  g matrix, still need to fix issues

--- acel_research_log/optimization/gmat.m
+++ acel_research_log/optimization/gmat.m
@@ -1,76 +1,24 @@
 function  gmat
-tic
-% discretization parameters
-p = 1;
-pquad = 2*p;
-nelem = 10;
+
+setup;
 mu = 0.01;
 
-% equation parameter
-ffun = @(x) ones(size(x,1),1);
+u = fem_p2(25, mu);
 
-% setup reference element
-ref = mkref1d(p,pquad);
+K = (size(u, 1) - 1)/2;
+x = linspace(0, 1, size(u, 1))';
 
-% make mesh
-mesh = mkmesh1d(nelem);
-mesh = meshlin2p1d(mesh,ref);
-mesh.bgrp = mkbgrp1d(mesh);
+ww = zeros(K * 3, 1);
+gg = zeros(K * 3, size(u, 1));
 
-% useful parameters
-nshp = size(ref.shp,2);
-
-% compute and store local matrices
-amat = zeros(nshp,nshp,nelem);
-imat = zeros(nshp,nshp,nelem);
-jmat = zeros(nshp,nshp,nelem);
-fvec = zeros(nshp,nelem);
-ivec = zeros(nshp,nelem);
-for elem = 1:nelem
-    % get dof indices
-    tril = mesh.tri(elem,:).';
-    
-    % compute mesh jacobians
-    xl = mesh.coord(tril,:);
-    xq = ref.shp*xl;
-    jacq = ref.shpx(:,:,1)*xl;
-    detJq = jacq;
-    ijacq = 1./jacq;
-    
-    % compute quadrature weight
-    wqJ = ref.wq.*detJq;
-    
-    % compute basis
-    phiq = ref.shp;
-    
-    % compute stiffness matrix
-    aaloc = mu*phixq(:,:,1)'*diag(wqJ)*phixq(:,:,1) + phiq(:,:,1)'*diag(wqJ)*phiq(:,:,1);
- 
-    % insert to global matrix
-    amat(:,:,elem) = aaloc;
-    imat(:,:,elem) = repmat(tril,[1,nshp]);
-    jmat(:,:,elem) = repmat(tril',[nshp,1]);
-    fvec(:,elem) = ffloc;
-    ivec(:,elem) = tril;
+for i = 1:2:size(x, 1) - 2
+    [w, g] = wqJ2(x(i), x(i+2));
+    ww(i:i + 2) = w;
+    gg((i+1)/2, i:i+2) = w' * g;
 end
 
-% assemble matrix
+disp(ww' * gg * u)
 
-A = sparse(imat(:),jmat(:),amat(:),ndof,ndof);
+ftrue = @(x) u_true(x, mu);
 
-% identify internal and boundary nodes
-bnodes = nodesonbgrp(mesh, [1,2]);
-inodes = setdiff((1:ndof)', bnodes);
-
-% solve linear system
-U = zeros(ndof,1);
-
-toc
-% plot solution
-if (1)
-figure(1), clf,
-plotfield1d(mesh,ref,U);
-end
-
-end
-
+disp(composite_gauss2(ftrue, 0, 1, 10))
--- acel_research_log/optimization/wqJ2.m
+++ acel_research_log/optimization/wqJ2.m
@@ -1,13 +1,19 @@
-function wqJ = wqJ2(n, a, b)
+function [w, phi] = wqJ2(a, b)
+    
+    
     c = (b + a)/2;
-    K = [a c b];
+    K = [a;c;b];
     V = [ones(3, 1) K K.^2];
     %get all the coeffs for the basis poly: 3x3 matrix
     C = eye(3) / V;
     
-    w = [-sqrt(3/5) 0 sqrt(3/5)]';
-    x = [5/9 8/9 5/9]';
-    
+    x0 = [-sqrt(3/5); 0; sqrt(3/5)];
+    w0 = [5/9; 8/9; 5/9];
+
+    x = a + (b - a)/2 * (x0 + 1);
+    w = (b - a)/2 * w0;
+
+    phi = [ones(3, 1) x x.^2] * C;
 
 end
 
