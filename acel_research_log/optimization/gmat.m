e8c13c1ad028bdde4f52dc6c534ebc7bb4950c1f
qed11 <ericdai1130@gmail.com>
2022-06-24 16:24:38 -0400

    wrong dimension of the w matrix, need fix

--- acel_research_log/optimization/gmat.m
+++ acel_research_log/optimization/gmat.m
@@ -2,14 +2,15 @@ function  gmat
 
 setup;
 mu = 0.01;
+n =3;
 
-u = fem_p2(25, mu);
+u = fem_p2(n, mu);
 
-K = (size(u, 1) - 1)/2;
+K = n+1;
 x = linspace(0, 1, size(u, 1))';
 
-ww = zeros(K * 3, 1);
-gg = zeros(K * 3, size(u, 1));
+ww = zeros(K, 1);
+gg = zeros(K, size(u, 1));
 
 for i = 1:2:size(x, 1) - 2
     [w, g] = wqJ2(x(i), x(i+2));
@@ -17,7 +18,8 @@ for i = 1:2:size(x, 1) - 2
     gg((i+1)/2, i:i+2) = w' * g;
 end
 
-disp(ww' * gg * u)
+disp(ww)
+disp(gg * u)
 
 ftrue = @(x) u_true(x, mu);
 
