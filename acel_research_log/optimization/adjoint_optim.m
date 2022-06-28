fc86bbc3f928299ff47b28dd684f392a69c7f44f
qed <ericdai1130@gmail.com>
2022-06-27 21:33:53 -0400

    finished optimization code using gradient descent, super duper uber slow, attempt to make it faster using other methods like fminunc

--- acel_research_log/optimization/adjoint_optim.m
+++ acel_research_log/optimization/adjoint_optim.m
@@ -1,21 +1,29 @@
 function adjoint_optim
+    tic
     mu_true = 1e-4 + (1e-1 - 1e-4)*rand;
     ftrue = @(x) 1 - u_true(x, mu_true);
     starget = composite_gauss2(ftrue, 0 , 1 , 10);
     
-    tol = 1e-4;
-    step = 0.1;
-    mu = 1e-4;
+    tol = 1e-7;
+    step = 0.01;
+    mu = 5e-2;
+    counter = 0;
     while true
+        counter = counter + 1;
+        disp(["Iteration:", counter, "mu = ", mu])
         dJ = dJdmu(starget, 9, mu);
 
-        curr_step = step/dJ;
-        mu = mu - curr_step * dJ;
+        mu = mu - step * dJ;
 
-        if abs(dJ) < tol || mu < 1e-4 || mu > 0.1
+        if abs(step* dJ) < tol  
             break
+        elseif mu > 0.1
+            mu = 0.075;
+        elseif mu < 1e-4
+            mu = 0.00025;
         end
     end
 
     disp([mu,mu_true])
+    toc
 end
