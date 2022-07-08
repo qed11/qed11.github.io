46e772aad3ed9dcf28f26890bd49d327ebec9d2e
qed11 desktop windows <ericdai1130@gmail.com>
2022-07-07 23:05:15 -0400

    changed demo slightly

--- acel_research_log/nonlin/demo.m
+++ acel_research_log/nonlin/demo.m
@@ -4,15 +4,15 @@ mu = 0.01;
 
 [u, fu, dfu] = fem_p2(n, mu);
 x = linspace(0, 1, size(u, 1) + 2);
+u_nonlin = fem_nonlin(n, mu, 1e-7);
 
 figure
 plot(x, [0;u;0]);
 hold on
-u_nonlin = fem_nonlin(n, mu, 1e-7);
 plot(x, [0; u_nonlin; 0])
 legend("linear", "nonlinear", "location", "best")
-
-[ww, phi] = gmat(n);
-g = ww' * phi;
-
-s = g * u_nonlin;
+title(sprintf("FE approximation on linear and nonlinear reaction - diffusion models, mu = %.5f", mu))
+xlabel("\(x\)", "Interpreter","latex")
+ylabel("\(u(x; \mu)\)","Interpreter","latex")
+saveas(gcf, "figs/demo.pdf")
+hold off
