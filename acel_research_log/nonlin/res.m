4ff4f3eb1e694ff245ab4014f125ba13b0c55d7a
qed11 <ericdai1130@gmail.com>
2022-07-11 15:37:22 -0400

    debugging jacobian of the residual; added some course notes for courses teaching too

--- acel_research_log/nonlin/res.m
+++ acel_research_log/nonlin/res.m
@@ -10,13 +10,20 @@ function [r, J] = res(n, mu, u)
     
     % computation of the residual -- derivation on onenote
     r = mu * A2 * u + ((phi * u).^3 .* phi)' * w - L;
+    %r = mu * A2 * u;% + phi' * ((phi * u).^3 .* w) - L;
     
     % helper function that creates a matrix that is convenient for
     % integration
-    wq = 3*(phi * u).^2;
+    wq = 3 *  w .*  (phi * u).^2;
+        
+    %wq = 3 * (phi * u).^2;
+
+
+
     wqs = spdiags(wq, 0, size(wq, 1),size(wq, 1));
     %computation of gradient of r wrt u - dR/du
-    J = mu * A2 + phi' * wqs * phi;
+    
+    J = mu * A2 +  phi' * wqs * phi ;
 
 end
 
