c620781086f2c8a6249bf24dbc6d9edbc6ff3d79
qed11 <ericdai1130@gmail.com>
2022-09-01 13:30:12 -0400

    final presentation slides set

--- acel_research_log/aso/error_conv_p1.m
+++ acel_research_log/aso/error_conv_p1.m
@@ -3,9 +3,9 @@ optim = [0.0178 0.001046 0.000186 9.81e-5]';
 param = [8.14e-2 1.05e-2 3.211e-3 4.84e-3]';
 
 figure
-loglog(dof, optim)
+loglog(dof, optim, "o-")
 hold on
-loglog(dof, param)
+loglog(dof, param, "+-")
 loglog(dof, 10*sqrt(1./(100.*dof)),"-.")
 loglog(dof, 10./dof,"-.")
 loglog(dof, 1e5./(dof.^2),"-.")
@@ -14,5 +14,5 @@ xlabel("Degrees of Freedom")
 ylabel("Error")
 legend("Pressure Matching Error", "Parameter Error Norm", ...
     "\(N^{-1/2}\) Convergence", "\(N^{-1}\) Convergence", ...
-    "\(N^{-2}\) Convergence", "Location","best")
-saveas(gcf, "uniform_mesh_figures/error_conv_p1.pdf")
+    "\(N^{-2}\) Convergence", "Location","best", "Interpreter", 'latex')
+saveas(gcf, "result_uniform_mesh/error_conv_p1.pdf")
--- acel_research_log/aso/error_conv_p2.m
+++ acel_research_log/aso/error_conv_p2.m
@@ -4,20 +4,20 @@ param = [6.57e-3 6.485e-4 3.234e-4 1.402e-4];
 
 
 figure
-loglog(dof, optim)
+loglog(dof, optim, "o-", MarkerSize=5)
 hold on
-loglog(dof, param)
+loglog(dof, param, "+-")
 
 % =====================================================
-%{
-adapt_err = readtable("error_csv/errors_orig_1.csv");
-loglog(adapt_err{:, 1}, adapt_err{:, 2})
-loglog(adapt_err{:, 1}, adapt_err{:, 3})
 
-adapt_err = readtable("error_csv/errors.csv");
-loglog(adapt_err{:, 1}, adapt_err{:, 2})
-loglog(adapt_err{:, 1}, adapt_err{:, 3})
-%}
+adapt_err = readtable("error_csv/errors_orig_1.csv");
+loglog(adapt_err{:, 1}, adapt_err{:, 2}, "*-")
+loglog(adapt_err{:, 1}, adapt_err{:, 3},"x-")
+
+adapt_err = readtable("error_csv/errors_orig_2.csv");
+loglog(adapt_err{:, 1}, adapt_err{:, 2}, "^-")
+loglog(adapt_err{:, 1}, adapt_err{:, 3}, "p-")
+
 %======================================================
 loglog(dof, sqrt(1./(100.*dof)),"-.")
 loglog(dof, 5./dof,"-.")
@@ -27,7 +27,8 @@ loglog(dof, 0.5./dof.^(2/3), "-.")
 title("Error convergence rate on uniformly spaced mesh", "Interpreter","latex")
 xlabel("Degrees of Freedom, \(N\)", "Interpreter","latex")
 ylabel("Error", "Interpreter","latex")
-legend("Pressure Matching Error", "Parameter Error Norm", ...
+legend("\(C_p\) error, uniform", "\(\mu\) Error, uniform", ...
+       "\(C_p\) error, AMR with init fineness 1 ","\(\mu\) Error, AMR with init fineness 1",...
        "\(N^{-1/2}\) Convergence", "\(N^{-1}\) Convergence", ...
        "\(N^{-2/3}\) Convergence","Location","best", 'Interpreter', "latex")
-saveas(gcf, "result_uniform_mesh/error_conv_p2.pdf")
+saveas(gcf, "result_uniform_mesh/error_conv_with_r1_adapt.pdf")
